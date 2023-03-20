f46e6d57630fada03549c75f5e8e891354cd30d2
Amirreza <riahiamirreza79@gmail.com>
2023-03-20 15:08:16 +0330

    Added unittests for env

---
+++ test/functional/environment/test_env.py
@@ -0,0 +1,25 @@
+import unittest
+
+from src.environments import Env
+
+class TestEnv(unittest.TestCase):
+    def test_read_env_01(self):
+        Env._init_envs_(env_file_path="test/functional/env/.env.test.1", keys=("TEST_ENV_VARIABLE_1",))
+        self.assertEqual(Env.TEST_ENV_VARIABLE_1, "hello")
+
+    def test_read_env_02(self):
+        Env._init_envs_(env_file_path="test/functional/env/.env.test.1", keys=("TEST_ENV_VARIABLE_1",))
+        with self.assertRaises(KeyError):
+            self.assertEqual(Env.TEST_ENV_VARIABLE_2, "hello")
+
+    def test_read_env_03(self):
+        with self.assertRaises(Exception):
+                Env()  #  trying to instantiating the class
+
+    def test_read_env_04(self):
+        Env._init_envs_(env_file_path="test/functional/env/.env.test.1")
+        self.assertEqual(Env.TEST_DB_NAME, "dbname")
+
+
+if __name__ == '__main__':
+    unittest.main()
