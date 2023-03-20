9bbe8c608547d186dbffaf91324a4383f86707f1
Amirreza <riahiamirreza79@gmail.com>
2023-03-20 14:34:08 +0330

    Implemented Env class with very basic functionality, wrote simple testcases

--- src/environments.py
+++ src/environments.py
@@ -1,15 +1,26 @@
 import dotenv
 
-class Env:
+class EnvMeta(type):
+
+    def __getattr__(cls, attr):
+        return cls._envs_dict[attr]
+
+
+class Env(metaclass=EnvMeta):
     env_file_path = ".env"
     keys = (
-        'TEST_DB_NAME'
+        'TEST_DB_NAME',
     )
 
+    def __new__(cls, *args, **kwargs):
+        raise Exception(
+            "This class must not be instantiated."
+        )
+
     @classmethod
-    def _init_envs_(cls, env_file_path=None):
+    def _init_envs_(cls, env_file_path=None, keys=None):
         envs = dotenv.dotenv_values(env_file_path or cls.env_file_path)
         cls._envs_dict = {}
-        for key in cls.keys:
+        for key in keys or cls.keys:
             cls._envs_dict[key] = envs[key]
 
