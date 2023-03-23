07aa87deb69f3b82a1bdfccb90d724973f44f97c
Amirreza <riahiamirreza79@gmail.com>
2023-03-23 14:37:16 +0430

    globalizing the node-manager object

--- src/api.py
+++ src/api.py
@@ -1,25 +1,26 @@
-from flask import Flask, jsonify, request, Response
+from flask import Flask, request, Response
 
 from src.node import NodeManager
 
 app = Flask(__name__)
 
+node_manager = NodeManager()
+
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify({'status': '200', 'content': 'pong'})
+    return Response(b'{"content": "pong"}', status=200, mimetype='application/json')
 
 
 @app.route('/node', methods=['POST'])
 def create_node():
     body = request.get_json()
     name = body['name']
-    node_id = NodeManager().create(name)
+    node_id = node_manager.create(name)
     return Response(f'{{"node_id": {node_id}}}'.encode(), status=201, mimetype='application/json')
 
 
 @app.route('/node/<node_id>', methods=['GET'])
 def get_node(node_id):
-    node = NodeManager().get(node_id=node_id)
-    print(node)
+    node = node_manager.get(node_id=node_id)
     return Response(f'{{"node": "{node}"}}'.encode(), status=200, mimetype='application/json')
--- src/node.py
+++ src/node.py
@@ -3,11 +3,11 @@ from src.db.sqlite import NodeSQLiteWrapper
 
 class NodeManager:
     def __init__(self):
-        self.node_db_wrapper = NodeSQLiteWrapper()
+        self.db_wrapper = NodeSQLiteWrapper()
 
     def create(self, name: str):
-        node_id = self.node_db_wrapper.insert(name, commit=True)
+        node_id = self.db_wrapper.insert(name, commit=True)
         return node_id
 
     def get(self, node_id):
-        return self.node_db_wrapper.fetch(id_=node_id)
+        return self.db_wrapper.fetch(id_=node_id)
