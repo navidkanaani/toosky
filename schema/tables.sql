a1948fc90f56f33734f0c6366c88dd84f0753398
navidkanaani <navidkaen@gmail.com>
2023-04-12 14:00:59 +0330

    Fix schema typo.

--- schema/tables.sql
+++ schema/tables.sql
@@ -21,16 +21,16 @@ CREATE TABLE IF NOT EXISTS FilterTable (
     threshold_type INTEGER CHECK(threshold_type in (0, 1)),
     threshold_value INTEGER,
 
-    rule_id INTEGER NOT NULL,
+    rule_eid INTEGER NOT NULL,
     
-    FOREIGN KEY (rule_id) REFERENCES RuleTable(rowid)
+    FOREIGN KEY (rule_eid) REFERENCES RuleTable(rowid)
 );
 
 CREATE TABLE IF NOT EXISTS WordTable (
     eid VARCHAR(31) NOT NULL UNIQUE,
     word VARCHAR(255) NOT NULL,
-    filter_id INTEGER,
-    FOREIGN KEY (filter_id) REFERENCES FilterTable(rowid)
+    filter_eid INTEGER,
+    FOREIGN KEY (filter_eid) REFERENCES FilterTable(rowid)
 );
 
 CREATE TABLE IF NOT EXISTS RuleTable (
@@ -52,11 +52,11 @@ CREATE TABLE IF NOT EXISTS NodeTable (
 
 -- CREATE TABLE IF NOT EXISTS NodeRelationTable (
 --     eid VARCHAR(31) NOT NULL UNIQUE,
---     parent_id INTEGER NOT NULL,
---     child_id INTEGER NOT NULL,
---     FOREIGN KEY (parent_id) REFERENCES NodeTable(rowid),
---     FOREIGN KEY (child_id) REFERENCES NodeTable(rowid),
---     UNIQUE (parent_id, child_id),
+--     parent_eid INTEGER NOT NULL,
+--     child_eid INTEGER NOT NULL,
+--     FOREIGN KEY (parent_eid) REFERENCES NodeTable(rowid),
+--     FOREIGN KEY (child_eid) REFERENCES NodeTable(rowid),
+--     UNIQUE (parent_eid, child_eid),
 -- );
 
 COMMIT;
